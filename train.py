3ec582358d263c6e204273c0005593306d8e6ad0
Melody Liu <melodyliu8282@gmail.com>
2026-06-16 22:19:09 -0700

    Add metric logging and update train.py

--- train.py
+++ train.py
@@ -5,11 +5,10 @@ import pytorch_lightning as pl
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning.loggers import CSVLogger
-from torch.utils.data import DataLoader
 
 # Local Imports
 from dataset import PTBXLDataset
-from model import ECGClassificationTask
+from classifier import ECGClassificationTask
 
 def train(
     save_dir="results",
@@ -27,15 +26,6 @@ def train(
     accelerator="auto",
 ):
     """Run the pipeline end-to-end: Download, build loaders, train and evaluate."""
-
-    # -- Datasets & loaders --
-    train_dataset = PTBXLDataset(csv_path=csv_path, data_root=data_root, split="train", leads=leads)
-    val_dataset   = PTBXLDataset(csv_path=csv_path, data_root=data_root, split="val",   leads=leads, mlb=train_dataset.mlb)
-    test_dataset  = PTBXLDataset(csv_path=csv_path, data_root=data_root, split="test",  leads=leads, mlb=train_dataset.mlb)
-
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,  num_workers=num_workers)
-    val_loader   = DataLoader(val_dataset,   batch_size=batch_size, shuffle=False, num_workers=num_workers)
-    test_loader  = DataLoader(test_dataset,  batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
     # -- Model --
     num_leads = 12 if leads is None else len(leads)
@@ -77,7 +67,7 @@ def train(
         logger=logger,
         enable_progress_bar=False, # in Colab the progress bar doesn't display correctly
     )
-    trainer.fit(task, train_loader, val_loader)
+    trainer.fit(task)
 
     best_path = ckpt_callback.best_model_path
 
@@ -86,36 +76,8 @@ def train(
     
     trainer.test(
         model=task,
-        dataloaders=test_loader,
         ckpt_path=best_path
     )
-
-def test(
-    exp_name,
-    save_dir="results",
-    csv_path="processed_ptbxl_metadata.csv",
-    data_root=".",
-    leads=None,
-    batch_size=32,
-    num_workers=0,
-    accelerator="auto",
-):
-    """Run an isolated evaluation loop using an established checkpoint folder."""
-    ckpt_dir  = os.path.join(save_dir, exp_name, "ckpts")
-    ckpt_path = glob.glob(os.path.join(ckpt_dir, "*.ckpt"))
-
-    if not ckpt_path:
-        raise ValueError(f"No checkpoint found in {ckpt_dir}")
-    ckpt_path = ckpt_path[0]
-
-    task = ECGClassificationTask.load_from_checkpoint(ckpt_path)
-
-    train_dataset = PTBXLDataset(csv_path=csv_path, data_root=data_root, split="train", leads=leads)
-    test_dataset  = PTBXLDataset(csv_path=csv_path, data_root=data_root, split="test",  leads=leads, mlb=train_dataset.mlb)
-    test_loader   = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-
-    trainer = Trainer(accelerator=accelerator)
-    trainer.test(task, test_loader)
 
 if __name__ == "__main__":
     fire.Fire(train)
